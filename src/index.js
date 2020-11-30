a8dacdcfef636f99ab675bb77f8ef71c4b9b19a5
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-30 23:59:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('input[type="submit"]').addEventListener("submit", function(event) {
+    event.preventDefault();
+  }, false);
+
 });
